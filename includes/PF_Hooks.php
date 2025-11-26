5a6552486b72aa802099e3507eac3f54c9798def
gesinn-it-gea <gesinn-it-gea@gesinn.it>
2025-11-26 16:25:58 +0100

    fix: set version to 5.5.1.0-alpha1

--- includes/PF_Hooks.php
+++ includes/PF_Hooks.php
@@ -23,7 +23,7 @@ class PFHooks {
 			return 1;
 		}
 
-		define( 'PF_VERSION', '5.5.1' );
+		define( 'PF_VERSION', '5.5.1.0-alpha1' );
 
 		$GLOBALS['wgPageFormsIP'] = dirname( __DIR__ ) . '/../';
 
