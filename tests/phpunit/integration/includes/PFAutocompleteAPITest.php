9eb0894d86bed4e9a194f7b17e6dc6d5ffd4091c
gesinn-it-gea <gesinn-it-gea@gesinn.it>
2026-03-16 21:57:11 +0100

    test(PFAutocompleteAPI): add initial PHPUnit and JSON test coverage

---
+++ tests/phpunit/integration/includes/PFAutocompleteAPITest.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * @covers \PFAutocompleteAPI
+ * @group PF
+ * @group Database
+ */
+class PFAutocompleteAPITest extends ApiTestCase {
+
+	/**
+	 * @covers \PFAutocompleteAPI::shiftExactMatch
+	 */
+	public function testShiftExactMatchMovesExactMatchToFront() {
+		$values = [ 'foobar', 'foo', 'foobaz' ];
+		$result = PFAutocompleteAPI::shiftExactMatch( 'foo', $values );
+		$this->assertSame( [ 'foo', 'foobar', 'foobaz' ], array_values( $result ) );
+	}
+
+	/**
+	 * @covers \PFAutocompleteAPI::shiftExactMatch
+	 */
+	public function testShiftExactMatchLeavesArrayUnchangedWhenNoMatch() {
+		$values = [ 'foobar', 'foobaz' ];
+		$result = PFAutocompleteAPI::shiftExactMatch( 'foo', $values );
+		$this->assertSame( [ 'foobar', 'foobaz' ], array_values( $result ) );
+	}
+
+	/**
+	 * @covers \PFAutocompleteAPI::shiftExactMatch
+	 */
+	public function testShiftExactMatchLeavesArrayUnchangedWhenMatchIsAlreadyFirst() {
+		$values = [ 'foo', 'foobar', 'foobaz' ];
+		$result = PFAutocompleteAPI::shiftExactMatch( 'foo', $values );
+		$this->assertSame( [ 'foo', 'foobar', 'foobaz' ], array_values( $result ) );
+	}
+
+	/**
+	 * @covers \PFAutocompleteAPI::shiftExactMatch
+	 */
+	public function testShiftExactMatchWithEmptyArray() {
+		$result = PFAutocompleteAPI::shiftExactMatch( 'foo', [] );
+		$this->assertSame( [], $result );
+	}
+
+	/**
+	 * @covers \PFAutocompleteAPI::execute
+	 */
+	public function testExecuteThrowsErrorWhenSubstrIsMissing() {
+		$this->expectException( ApiUsageException::class );
+		$this->doApiRequest( [
+			'action' => 'pfautocomplete',
+		] );
+	}
+
+}
