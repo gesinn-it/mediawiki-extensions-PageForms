947e40b9412d4fdd4f8233138f7b15b5c8eeb3a7
gesinn-it-gea <gesinn-it-gea@gesinn.it>
2026-03-19 13:48:40 +0100

    test(coverage): extend PFTemplateField and PFForm test coverage

--- tests/phpunit/integration/includes/PF_FormTest.php
+++ tests/phpunit/integration/includes/PF_FormTest.php
@@ -37,4 +37,97 @@ class PFFormTest extends MediaWikiIntegrationTestCase {
 		$this->assertTrue( (bool)equalTo( $actual ) );
 		$this->assertEquals( $actual, $this->pfForm->getItems() );
 	}
+
+	public function testCreateNormalizesUnderscoresToSpaces() {
+		$form = PFForm::create( 'my_form', [] );
+		$this->assertSame( 'My form', $form->getFormName() );
+	}
+
+	public function testCreateAppliesUcfirst() {
+		$form = PFForm::create( 'test form', [] );
+		$this->assertSame( 'Test form', $form->getFormName() );
+	}
+
+	public function testCreateHandlesNullFormName() {
+		$form = PFForm::create( null, [] );
+		$this->assertSame( '', $form->getFormName() );
+	}
+
+	public function testCreateSetsItems() {
+		$form = PFForm::create( 'MyForm', [ 'item1', 'item2' ] );
+		$this->assertSame( [ 'item1', 'item2' ], $form->getItems() );
+	}
+
+	public function testCreateMarkupContainsForminput() {
+		$form = PFForm::create( 'My Form', [] );
+		$markup = $form->createMarkup();
+		$this->assertStringContainsString( '{{#forminput:form=My Form}}', $markup );
+	}
+
+	public function testCreateMarkupContainsNoinclude() {
+		$form = PFForm::create( 'My Form', [] );
+		$markup = $form->createMarkup();
+		$this->assertStringContainsString( '<noinclude>', $markup );
+		$this->assertStringContainsString( '</noinclude>', $markup );
+	}
+
+	public function testCreateMarkupContainsIncludeonly() {
+		$form = PFForm::create( 'My Form', [] );
+		$markup = $form->createMarkup();
+		$this->assertStringContainsString( '<includeonly>', $markup );
+		$this->assertStringContainsString( '</includeonly>', $markup );
+	}
+
+	public function testCreateMarkupWithAssociatedCategory() {
+		$form = PFForm::create( 'My Form', [] );
+		$form->setAssociatedCategory( 'MyCategory' );
+		$markup = $form->createMarkup();
+		$this->assertStringContainsString( '|autocomplete on category=MyCategory', $markup );
+	}
+
+	public function testCreateMarkupWithPageNameFormula() {
+		$form = PFForm::create( 'My Form', [] );
+		$form->setPageNameFormula( '{{PAGENAME}}' );
+		$markup = $form->createMarkup();
+		$this->assertStringContainsString( '|page name={{PAGENAME}}', $markup );
+	}
+
+	public function testCreateMarkupWithCreateTitle() {
+		$form = PFForm::create( 'My Form', [] );
+		$form->setCreateTitle( 'Add entry' );
+		$markup = $form->createMarkup();
+		$this->assertStringContainsString( '|create title=Add entry', $markup );
+	}
+
+	public function testCreateMarkupWithEditTitle() {
+		$form = PFForm::create( 'My Form', [] );
+		$form->setEditTitle( 'Edit entry' );
+		$markup = $form->createMarkup();
+		$this->assertStringContainsString( '|edit title=Edit entry', $markup );
+	}
+
+	public function testCreateMarkupWithoutFreeTextOmitsFreeTextInput() {
+		$form = PFForm::create( 'My Form', [] );
+		$markup = $form->createMarkup( false );
+		$this->assertStringNotContainsString( '{{{standard input|free text', $markup );
+	}
+
+	public function testCreateMarkupWithCustomFreeTextLabel() {
+		$form = PFForm::create( 'My Form', [] );
+		$markup = $form->createMarkup( true, 'Additional notes' );
+		$this->assertStringContainsString( 'Additional notes', $markup );
+		$this->assertStringContainsString( '{{{standard input|free text', $markup );
+	}
+
+	public function testCreateMarkupDefaultIncludesFreeText() {
+		$form = PFForm::create( 'My Form', [] );
+		$markup = $form->createMarkup();
+		$this->assertStringContainsString( '{{{standard input|free text', $markup );
+	}
+
+	public function testFormNameWithCommaEscapedInForminput() {
+		$form = PFForm::create( 'Form,Name', [] );
+		$markup = $form->createMarkup();
+		$this->assertStringContainsString( 'form=Form\,Name', $markup );
+	}
 }
--- tests/phpunit/integration/includes/PF_TemplateFieldTest.php
+++ tests/phpunit/integration/includes/PF_TemplateFieldTest.php
@@ -102,4 +102,175 @@ class PFTemplateFieldTest extends TestCase {
 		$expected = "testField (label=Custom Label;namespace=MyNamespace)";
 		$this->assertEquals( $expected, $result );
 	}
+
+	public function testToWikitextOmitsSameLabelAsFieldName() {
+		$field = PFTemplateField::create( 'testField', 'testField' );
+		$this->assertSame( 'testField', $field->toWikitext() );
+	}
+
+	public function testToWikitextIncludesListAttribute() {
+		$field = PFTemplateField::create( 'myField', null, null, true );
+		$this->assertStringContainsString( 'list', $field->toWikitext() );
+	}
+
+	public function testToWikitextIncludesCustomDelimiter() {
+		$field = PFTemplateField::create( 'myField', null, null, true, ';' );
+		$this->assertStringContainsString( 'delimiter=;', $field->toWikitext() );
+	}
+
+	public function testToWikitextOmitsDefaultCommaDelimiter() {
+		$field = PFTemplateField::create( 'myField', null, null, true, ',' );
+		$this->assertStringNotContainsString( 'delimiter', $field->toWikitext() );
+	}
+
+	public function testToWikitextIncludesProperty() {
+		$field = PFTemplateField::create( 'myField', null, 'SomeProp' );
+		$this->assertStringContainsString( 'property=SomeProp', $field->toWikitext() );
+	}
+
+	public function testToWikitextIncludesDisplay() {
+		$field = PFTemplateField::create( 'myField', null, null, null, null, 'table' );
+		$this->assertStringContainsString( 'display=table', $field->toWikitext() );
+	}
+
+	public function testToWikitextIncludesNSText() {
+		$field = PFTemplateField::create( 'myField', null );
+		$field->setNSText( 'User' );
+		$this->assertStringContainsString( 'namespace=User', $field->toWikitext() );
+	}
+
+	public function testNewFromParamsSetsFieldName() {
+		$field = PFTemplateField::newFromParams( 'MyField', [] );
+		$this->assertSame( 'MyField', $field->getFieldName() );
+	}
+
+	public function testNewFromParamsSetsLabel() {
+		$field = PFTemplateField::newFromParams( 'Field', [ 'label' => 'My Label' ] );
+		$this->assertSame( 'My Label', $field->getLabel() );
+	}
+
+	public function testNewFromParamsListDefaultsDelimiterToComma() {
+		$field = PFTemplateField::newFromParams( 'Field', [ 'list' => true ] );
+		$this->assertTrue( $field->isList() );
+		$this->assertSame( ',', $field->getDelimiter() );
+	}
+
+	public function testNewFromParamsSetsCustomDelimiter() {
+		$field = PFTemplateField::newFromParams( 'Field', [ 'list' => true, 'delimiter' => ';' ] );
+		$this->assertSame( ';', $field->getDelimiter() );
+	}
+
+	public function testNewFromParamsSetsHoldsTemplate() {
+		$field = PFTemplateField::newFromParams( 'Field', [ 'holds template' => 'TplName' ] );
+		$this->assertSame( 'TplName', $field->getHoldsTemplate() );
+	}
+
+	public function testNewFromParamsSetsCategory() {
+		$field = PFTemplateField::newFromParams( 'Field', [ 'category' => 'MyCategory' ] );
+		$this->assertSame( 'MyCategory', $field->getCategory() );
+	}
+
+	public function testNewFromParamsSetsDisplay() {
+		$field = PFTemplateField::newFromParams( 'Field', [ 'display' => 'table' ] );
+		$this->assertSame( 'table', $field->getDisplay() );
+	}
+
+	public function testNewFromParamsSetsNamespaceFromText() {
+		$field = PFTemplateField::newFromParams( 'Field', [ 'namespace' => 'User' ] );
+		$this->assertSame( 'User', $field->getNSText() );
+		$this->assertSame( NS_USER, $field->getNamespace() );
+	}
+
+	public function testDefaultIsMandatoryIsFalse() {
+		$field = PFTemplateField::create( 'Field', null );
+		$this->assertFalse( $field->isMandatory() );
+	}
+
+	public function testDefaultIsUniqueIsFalse() {
+		$field = PFTemplateField::create( 'Field', null );
+		$this->assertFalse( $field->isUnique() );
+	}
+
+	public function testDefaultRegexIsNull() {
+		$field = PFTemplateField::create( 'Field', null );
+		$this->assertNull( $field->getRegex() );
+	}
+
+	public function testDefaultHoldsTemplateIsNull() {
+		$field = PFTemplateField::create( 'Field', null );
+		$this->assertNull( $field->getHoldsTemplate() );
+	}
+
+	public function testDefaultCategoryIsNull() {
+		$field = PFTemplateField::create( 'Field', null );
+		$this->assertNull( $field->getCategory() );
+	}
+
+	public function testDefaultNamespaceIsZero() {
+		$field = PFTemplateField::create( 'Field', null );
+		$this->assertSame( 0, $field->getNamespace() );
+	}
+
+	public function testSetSemanticPropertyHandlesNull() {
+		$field = PFTemplateField::create( 'Field', null );
+		$field->setSemanticProperty( null );
+		$this->assertSame( '', $field->getSemanticProperty() );
+	}
+
+	public function testSetSemanticPropertyStripsBackslashes() {
+		$field = PFTemplateField::create( 'Field', null );
+		$field->setSemanticProperty( 'Some\\Prop' );
+		$this->assertSame( 'SomeProp', $field->getSemanticProperty() );
+	}
+
+	public function testSetSemanticPropertyClearsPossibleValues() {
+		$field = PFTemplateField::create( 'Field', null );
+		$field->setPossibleValues( [ 'a', 'b' ] );
+		$field->setSemanticProperty( 'NewProp' );
+		$this->assertSame( [], $field->getPossibleValues() );
+	}
+
+	public function testSetAndGetPossibleValues() {
+		$field = PFTemplateField::create( 'Field', null );
+		$field->setPossibleValues( [ 'Alpha', 'Beta' ] );
+		$this->assertSame( [ 'Alpha', 'Beta' ], $field->getPossibleValues() );
+	}
+
+	public function testGetPossibleValuesReturnsEmptyArrayAfterCreate() {
+		$field = PFTemplateField::create( 'Field', null );
+		$this->assertSame( [], $field->getPossibleValues() );
+	}
+
+	public function testGetExpectedCargoFieldFallsBackToUnderscoreFieldName() {
+		$field = PFTemplateField::create( 'My Field', null );
+		$this->assertSame( 'My_Field', $field->getExpectedCargoField() );
+	}
+
+	public function testGetFullCargoFieldReturnsNullWhenNotSet() {
+		$field = PFTemplateField::create( 'Field', null );
+		$this->assertNull( $field->getFullCargoField() );
+	}
+
+	public function testSetFieldTypeSetsType() {
+		$field = PFTemplateField::create( 'Field', null );
+		$field->setFieldType( 'Text' );
+		$this->assertSame( 'Text', $field->getFieldType() );
+	}
+
+	public function testSetNSTextWithKnownNamespaceSetsNamespaceId() {
+		$field = PFTemplateField::create( 'Field', null );
+		$field->setNSText( 'User' );
+		$this->assertSame( 'User', $field->getNSText() );
+		$this->assertSame( NS_USER, $field->getNamespace() );
+	}
+
+	public function testCreateStripsBackslashesFromFieldName() {
+		$field = PFTemplateField::create( 'Field\\Name', null );
+		$this->assertSame( 'FieldName', $field->getFieldName() );
+	}
+
+	public function testCreateTrimsFieldName() {
+		$field = PFTemplateField::create( '  trimmed  ', null );
+		$this->assertSame( 'trimmed', $field->getFieldName() );
+	}
 }
